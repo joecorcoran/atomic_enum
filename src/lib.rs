21c56a96b0d45e33d4ade02f83d73a005480daea
Joe Corcoran <joe@corcoran.online>
2020-10-08 14:51:26 +0200

    Update lints
    To avoid incompatible forbid/warn.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![forbid(
     rust_2018_idioms,
     future_incompatible,
-    elided_lifetimes_in_paths,
     unsafe_code
 )]
 #![warn(
@@ -9,10 +8,7 @@
     missing_docs,
     trivial_casts,
     trivial_numeric_casts,
-    unreachable_pub,
-    unused_extern_crates,
-    unused_import_braces,
-    unused_qualifications
+    unused
 )]
 
 //! An attribute to create an atomic wrapper around a C-style enum.
